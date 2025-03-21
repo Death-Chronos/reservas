ef832df329f438bff2207ba41f90639aa763e0bf
JV <joaovictorcunha12102002@gmail.com>
2025-03-21 16:17:14 -0300

    controller e service basico

---
+++ src/main/java/com/reservas/controllers/QuartoController.java
@@ -0,0 +1,53 @@
+package com.reservas.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.reservas.models.Quarto;
+import com.reservas.services.QuartoService;
+
+import jakarta.validation.Valid;
+
+@Controller
+@RequestMapping("/quarto")
+public class QuartoController {
+
+    @Autowired
+    QuartoService quartoService;
+    
+    @PostMapping("/criar")
+    public ResponseEntity<Quarto> criarQuarto(@RequestBody @Valid Quarto quarto){
+        return ResponseEntity.status(HttpStatus.CREATED).body(quartoService.criarQuarto(quarto));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Quarto> getQuartoPorId(@PathVariable Long id){
+        return ResponseEntity.status(HttpStatus.OK).body(quartoService.getQuartoPorId(id));
+    }
+    @GetMapping("/all")
+    public ResponseEntity<List<Quarto>> getAllQuartos(){
+        return ResponseEntity.status(HttpStatus.OK).body(quartoService.getAllQuartos());
+    }
+
+    @PutMapping("/atualizar/{id}")
+    public ResponseEntity<Quarto> atualizarQuarto(@PathVariable Long id, @RequestBody @Valid Quarto quarto ){
+        return ResponseEntity.status(HttpStatus.OK).body(quartoService.atualizarQuarto(id, quarto));
+    }
+
+    @DeleteMapping("/deletar/{id}")
+    public ResponseEntity<Void> deletarQuarto(@PathVariable Long id){
+        quartoService.deletarQuarto(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+}
--- src/main/java/com/reservas/services/QuartoService.java
+++ src/main/java/com/reservas/services/QuartoService.java
@@ -1,5 +1,8 @@
 package com.reservas.services;
 
+import java.util.List;
+
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -22,4 +25,26 @@ public class QuartoService {
             ()-> new ResourceNotFoundException("Quarto não encontrado com o id: " + id));
     }
 
+    public List<Quarto> getAllQuartos(){
+        return quartoRepo.findAll();
+    }
+
+    public Quarto atualizarQuarto(Long id, Quarto updatedQuarto) {
+        if (quartoExisteById(id)) {
+            Quarto quarto = getQuartoPorId(id);
+            BeanUtils.copyProperties(updatedQuarto, quarto);
+            return quartoRepo.save(quarto);
+        }
+        throw new ResourceNotFoundException("Quarto não encontrado com o id: " + id);
+    }
+
+    public void deletarQuarto(Long id){
+        if(quartoExisteById(id)){
+            quartoRepo.deleteById(id);
+        }
+            throw new ResourceNotFoundException("Quarto não encontrado com o id: " + id);        
+    }
+    private Boolean quartoExisteById(Long id){
+        return quartoRepo.existsById(id);
+    }
 }
