afbfdc0a303f8edaca76632e8fb7c84246b4210f
Death-Chronos <80175567+Death-Chronos@users.noreply.github.com>
2025-05-07 17:28:08 -0300

    Retirando importaçoes desnecessárias

--- src/test/java/com/reservas/services/ReservaServiceTest.java
+++ src/test/java/com/reservas/services/ReservaServiceTest.java
@@ -8,7 +8,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 
 import java.time.LocalDate;
